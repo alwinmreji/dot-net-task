ecf54b7e4d4ad71e1df123797e068ccd8af2cd80
Alwin.Reji <alwin.reji@falconautoonline.com>
2023-10-09 19:57:12 +0530

    added dtos for two pages

---
+++ Program/Models/ApplicationFormDto.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program.Models
+{
+    public class ApplicationFormDto
+    {
+        public string UploadedCoverImage { get; set; }
+        public PersonalInformationDto PersonalInformation { get; set; }
+        public ProfileInformationDto ProfileInformation { get; set; }
+        public List<QuestionsDto> AdditionalQuestionList { get; set; }
+
+    }
+    public class InternalInformationDto<T> where T :  class
+    {
+        public T Value { get; set; }
+        public bool? IsInternal { get; set; }
+        public bool IsHidden { get; set; }
+        public bool? IsMandate { get; set; }
+    }
+    public class QuestionsDto
+    {
+        public string Type { get; set; }
+        public string Question { get; set; }
+    }
+    public class PersonalInformationDto
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public InternalInformationDto<string> Phone { get; set; }
+        public InternalInformationDto<string> Nationality { get; set; }
+        public InternalInformationDto<string> CurrentResidence { get; set; }
+        public InternalInformationDto<string> IdNumber { get; set; }
+        public InternalInformationDto<string> DateOfBirth { get; set; }
+        public InternalInformationDto<string> Gender { get;set; }
+        public List<QuestionsDto> QuestionList { get; set; }
+    }
+    public class EducationDto
+    {
+        public string School { get; set; }
+        public string Degree { get; set; }
+        public string CourseName { get; set; }
+        public string Location { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public bool IsCurrentlyStudying { get; set; }
+    }
+    public class WorkExperienceDto
+    {
+        public string Company { get; set; }
+        public string Title { get; set; }
+        public string Location { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public bool IsCurrentlyWorking { get; set; }
+    }
+    public class ProfileInformationDto
+    {
+        public InternalInformationDto<EducationDto> EducationList { get; set; }
+        public InternalInformationDto<WorkExperienceDto> WorkExperienceList { get; set; }
+        public InternalInformationDto<string> Resume { get; set; }
+    }
+
+}
---
+++ Program/Models/ProgramDetailsDto.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program.Models
+{
+    public class ProgramDetailsDto
+    {
+        public string ProgramTitle { get; set; }
+        public string SummaryOfTheProgram { get; set; }
+        public string ProgramDescription { get; set; }
+        public List<string> KeySkillsRequired { get; set; }
+        public string ProgramBenefits { get; set; }
+        public string ApplicationCriteria { get; set; }
+        public ProgramDetailsAdditionalInformationDto ProgramAdditionalInformation { get; set; }
+    }
+    public class ProgramDetailsAdditionalInformationDto
+    {
+        public string ProgramType { get; set; }
+        public DateTime ProgramStart { get; set; }
+        public DateTime ApplicationOpen { get; set; }
+        public DateTime ApplicationClose { get; set; }
+        public int DurationInMonths { get; set; }
+        public string ProgramLocation { get; set; }
+        public string MinimumQualifications { get; set; }
+        public string MaximumNumberOfApplications { get; set; }
+    }
+}
