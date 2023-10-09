bc484dbeb6383c58789b617dbc4747c346c50708
Alwin.Reji <alwin.reji@falconautoonline.com>
2023-10-09 20:30:54 +0530

    workflow dto added to add different stages

---
+++ Program/Models/WorkFlowDto.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program.Models
+{
+    public class WorkFlowDto
+    {
+        public List<StageDto> StageList { get; set; }
+    }
+    public class StageDto
+    {
+        public string StageName { get; set; }
+        public bool IsShortlistingType { get; set; }
+        public VideoInterviewDto VideoInterview { get; set; }
+        public bool IsPlacement { get; set; }
+        public bool IsStageVisibleToCandidate { get; set; }
+    }
+    public class VideoInterviewDto
+    {
+        public bool IsVideoInterviewRequired { get; set; } = false;
+        public string Question { get; set; }
+        public string AdditionalInformation { get; set; }
+        public VideoDurationDto VideoDuration { get; set; }
+        public int DeadlineDaysCount { get; set; }
+
+    }
+    public class VideoDurationDto
+    {
+        public int MaxDurationOfVideo { get; set; }
+        public int Unit { get; set; }
+    }
+}
